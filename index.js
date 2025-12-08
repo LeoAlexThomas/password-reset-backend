3c31481440381b773d317a2425cfe8590573798e
LeoAlexThomas <leoalex960@gmail.com>
2025-12-08 19:36:22 +0530

    updated cors block in index.js

--- index.js
+++ index.js
@@ -14,7 +14,13 @@ const app = express();
 // Default middleware
 app.use(express.json());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
 
 // Connecting Database
 connectDB();
